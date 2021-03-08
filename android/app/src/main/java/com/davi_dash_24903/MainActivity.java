76fa7d085e18a1f92c6aeaac456a92002efc6e2d
Crowdbotics <team@crowdbotics.com>
2021-03-08 00:27:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/davi_dash_24903/MainActivity.java
@@ -0,0 +1,15 @@
+package com.davi_dash_24903;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "davi_dash_24903";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://davi-dash-24903.botics.co",
+  defaultTimeout: 5000
+};
